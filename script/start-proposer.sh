23eb266addf42fdd9e80ca2c7d66e400fc38c5dd
David <david@taiko.xyz>
2023-01-06 14:12:36 +0800

    feat(proposer): update proposer RPC config temporarily (#26)

--- script/start-proposer.sh
+++ script/start-proposer.sh
@@ -5,7 +5,7 @@ set -eou pipefail
 if [ "$ENABLE_PROPOSER" == "true" ]; then
     taiko-client proposer \
       --l1.ws ${L1_ENDPOINT_WS} \
-      --l2.ws ws://l2_execution_engine:8546 \
+      --l2.ws http://l2_execution_engine:8545 \
       --taikoL1 ${TAIKO_L1_ADDRESS} \
       --taikoL2 ${TAIKO_L2_ADDRESS} \
       --l1.proposerPrivKey ${L1_PROPOSER_PRIVATE_KEY} \
