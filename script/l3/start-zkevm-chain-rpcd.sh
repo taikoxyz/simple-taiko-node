a0e0a488872e89885fc9cfc7ea7662b708298aee
David <david@taiko.xyz>
2023-07-18 16:36:07 +0800

    feat: update script

--- script/l3/start-zkevm-chain-rpcd.sh
+++ script/l3/start-zkevm-chain-rpcd.sh
@@ -5,9 +5,7 @@ set -eou pipefail
 if [ "$ENABLE_PROVER" == "true" ]; then
     mkdir -p /data
 
-    if [ ! -f "/data/kzg_bn254_21.srs" ];then
-        wget https://storage.googleapis.com/zkevm-circuits-keys/kzg_bn254_21.srs -P /data
-    fi
+    rm -rf /data/kzg_bn254_21.srs && wget https://storage.googleapis.com/zkevm-circuits-keys/kzg_bn254_21.srs -P /data
 
     /prover_rpcd --bind 0.0.0.0:${PORT_ZKEVM_CHAIN_PROVER_RPCD}
 else
