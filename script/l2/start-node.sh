abbb4314a4a63d0a807ff8bf825e6b1ed0e245d7
liray-unendlich <unenlichkeit629@gmail.com>
2023-08-01 02:08:47 +0900

    Fix new line

--- script/l2/start-node.sh
+++ script/l2/start-node.sh
@@ -4,24 +4,24 @@ set -eou pipefail
 
 if [ "$START_L2_NODE" == "true" ]; then
     geth \
-        --taiko
-        --networkid="${L2_CHAIN_ID}"
-        --syncmode=full
-        --gcmode=archive
-        --datadir=/data/taiko-geth
-        --metrics
-        --metrics.expensive
-        --metrics.addr="0.0.0.0"
-        --bootnodes=${L2_BOOT_NODES}
-        --authrpc.addr="0.0.0.0"
-        --authrpc.vhosts="*"
-        --http
-        --http.api=debug,eth,net,web3,txpool,taiko
-        --http.addr="0.0.0.0"
-        --http.vhosts="*"
-        --ws
-        --ws.api=debug,eth,net,web3,txpool,taiko
-        --ws.addr="0.0.0.0"
+        --taiko \
+        --networkid="${L2_CHAIN_ID}" \
+        --syncmode=full \
+        --gcmode=archive \
+        --datadir=/data/taiko-geth \
+        --metrics \
+        --metrics.expensive \
+        --metrics.addr="0.0.0.0" \
+        --bootnodes=${L2_BOOT_NODES} \
+        --authrpc.addr="0.0.0.0" \
+        --authrpc.vhosts="*" \
+        --http \
+        --http.api=debug,eth,net,web3,txpool,taiko \
+        --http.addr="0.0.0.0" \
+        --http.vhosts="*" \
+        --ws \
+        --ws.api=debug,eth,net,web3,txpool,taiko \
+        --ws.addr="0.0.0.0" \
         --ws.origins="*"
 else
     sleep infinity
--- script/l3/start-node.sh
+++ script/l3/start-node.sh
@@ -4,25 +4,25 @@ set -eou pipefail
 
 if [ "$START_L3_NODE" == "true" ]; then
     geth \
-        --taiko
-        --networkid="${L3_CHAIN_ID}"
-        --syncmode=full
-        --gcmode=archive
-        --datadir=/data/taiko-geth
-        --metrics
-        --metrics.expensive
-        --metrics.addr="0.0.0.0"
-        --bootnodes=${L3_BOOT_NODES}
-        --authrpc.addr="0.0.0.0"
-        --authrpc.vhosts="*"
-        --http
-        --http.api=debug,eth,net,web3,txpool,taiko
-        --http.addr="0.0.0.0"
-        --http.vhosts="*"
-        --ws
-        --ws.api=debug,eth,net,web3,txpool,taiko
-        --ws.addr="0.0.0.0"
-        --ws.origins="*"
+        --taiko \
+        --networkid="${L3_CHAIN_ID}" \
+        --syncmode=full \
+        --gcmode=archive \
+        --datadir="/data/taiko-geth" \
+        --metrics \
+        --metrics.expensive \
+        --metrics.addr="0.0.0.0" \
+        --bootnodes=${L3_BOOT_NODES} \
+        --authrpc.addr="0.0.0.0" \
+        --authrpc.vhosts="*" \
+        --http \
+        --http.api=debug,eth,net,web3,txpool,taiko \
+        --http.addr="0.0.0.0" \
+        --http.vhosts="*" \
+        --ws \
+        --ws.api=debug,eth,net,web3,txpool,taiko \
+        --ws.addr="0.0.0.0" \
+        --ws.origins="*" 
 else
     sleep infinity
 fi
