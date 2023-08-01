138be6db9f2b901ab69a9ae292cd1864ba5157af
Jeffery Walsh <cyberhorsey@gmail.com>
2023-08-01 11:12:56 -0700

    wait receipt timeout flags + refactor prover scripts to be easier to add new flags

--- script/l2/start-prover-relayer.sh
+++ script/l2/start-prover-relayer.sh
@@ -10,17 +10,22 @@ if [ "$ENABLE_PROVER" == "true" ]; then
 
     WAIT_HOSTS=zkevm_chain_prover_rpcd:9000 WAIT_TIMEOUT=360 ./wait
 
-    taiko-client prover \
-        --l1.ws ${L1_ENDPOINT_WS} \
-        --l2.ws ws://l2_execution_engine:8546 \
-        --l1.http ${L1_ENDPOINT_HTTP} \
-        --l2.http http://l2_execution_engine:8545 \
-        --taikoL1 ${TAIKO_L1_ADDRESS} \
-        --taikoL2 ${TAIKO_L2_ADDRESS} \
-        --zkevmRpcdEndpoint http://zkevm_chain_prover_rpcd:9000 \
-        --zkevmRpcdParamsPath /data \
-        --l1.proverPrivKey ${L1_PROVER_PRIVATE_KEY} \
-        --maxConcurrentProvingJobs ${ZKEVM_CHAIN_INSTANCES_NUM}
+    ARGS="--l1.ws ${L1_ENDPOINT_WS}
+        --l2.ws ws://l2_execution_engine:8546
+        --l1.http ${L1_ENDPOINT_HTTP}
+        --l2.http http://l2_execution_engine:8545
+        --taikoL1 ${TAIKO_L1_ADDRESS}
+        --taikoL2 ${TAIKO_L2_ADDRESS}
+        --zkevmRpcdEndpoint http://zkevm_chain_prover_rpcd:9000
+        --zkevmRpcdParamsPath /data
+        --l1.proverPrivKey ${L1_PROVER_PRIVATE_KEY}
+        --maxConcurrentProvingJobs ${ZKEVM_CHAIN_INSTANCES_NUM}"
+
+    if [[ ! -z "$WAIT_RECEIPT_TIMEOUT" ]]; then
+        ARGS="${ARGS} --rpc.waitReceiptTimeout ${WAIT_RECEIPT_TIMEOUT}"
+    fi
+
+    taiko-client prover {$ARGS}
 else
     sleep infinity
 fi
--- script/l3/start-prover-relayer.sh
+++ script/l3/start-prover-relayer.sh
@@ -10,35 +10,31 @@ if [ "$ENABLE_PROVER" == "true" ]; then
 
     WAIT_HOSTS=l3_zkevm_chain_prover_rpcd:9000 WAIT_TIMEOUT=3600 ./wait
 
-    if [ "$PROVE_UNASSIGNED_BLOCKS" == "true" ]; then
-        taiko-client prover \
-        --l1.ws ${L2_ENDPOINT_WS} \
-        --l2.ws ws://l3_execution_engine:8546 \
-        --l1.http ${L2_ENDPOINT_HTTP} \
-        --l2.http http://l3_execution_engine:8545 \
-        --taikoL1 ${TAIKO_L1_ADDRESS} \
-        --taikoL2 ${TAIKO_L2_ADDRESS} \
-        --taikoProverPoolL1 ${PROVER_POOL_ADDRESS} \
-        --zkevmRpcdEndpoint http://l3_zkevm_chain_prover_rpcd:9000 \
+
+    ARGS="--l1.ws ${L2_ENDPOINT_WS}
+        --l2.ws ws://l3_execution_engine:8546
+        --l1.http ${L2_ENDPOINT_HTTP}
+        --l2.http http://l3_execution_engine:8545
+        --taikoL1 ${TAIKO_L1_ADDRESS}
+        --taikoL2 ${TAIKO_L2_ADDRESS}
+        --taikoProverPoolL1 ${PROVER_POOL_ADDRESS}
+        --zkevmRpcdEndpoint http://l3_zkevm_chain_prover_rpcd:9000
         --zkevmRpcdParamsPath /data \
-        --l1.proverPrivKey ${L2_PROVER_PRIVATE_KEY} \
-        --maxConcurrentProvingJobs ${ZKEVM_CHAIN_INSTANCES_NUM} \
-        --prover.proveUnassignedBlocks
-    else
-        taiko-client prover \
-        --l1.ws ${L2_ENDPOINT_WS} \
-        --l2.ws ws://l3_execution_engine:8546 \
-        --l1.http ${L2_ENDPOINT_HTTP} \
-        --l2.http http://l3_execution_engine:8545 \
-        --taikoL1 ${TAIKO_L1_ADDRESS} \
-        --taikoL2 ${TAIKO_L2_ADDRESS} \
-        --taikoProverPoolL1 ${PROVER_POOL_ADDRESS} \
-        --zkevmRpcdEndpoint http://l3_zkevm_chain_prover_rpcd:9000 \
-        --zkevmRpcdParamsPath /data \
-        --l1.proverPrivKey ${L2_PROVER_PRIVATE_KEY} \
-        --maxConcurrentProvingJobs ${ZKEVM_CHAIN_INSTANCES_NUM} \
-        --prover.proveUnassignedBlocks=false
+        --l1.proverPrivKey ${L2_PROVER_PRIVATE_KEY}
+        --maxConcurrentProvingJobs ${ZKEVM_CHAIN_INSTANCES_NUM}"
+
+    if [[ ! -z "$WAIT_RECEIPT_TIMEOUT" ]]; then
+        ARGS="${ARGS} --rpc.waitReceiptTimeout ${WAIT_RECEIPT_TIMEOUT}"
     fi
+
+    if [[ ! -z "$PROVE_UNASSIGNED_BLOCKS" ]]; then
+        ARGS="${ARGS} --prover.proveUnassignedBlocks"
+    else
+        ARGS="${ARGS} --prover.proveUnassignedBlocks false"
+    fi
+    
+
+    taiko-client prover ${ARGS}
 else
     sleep infinity
 fi
