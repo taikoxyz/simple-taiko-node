6b084e26be97b9c3566d94ed0eceaf33e88103e4
David <david@taiko.xyz>
2023-08-06 15:45:54 +0800

    feat(all): add default `proposeBlockTxGasTipCap` flag value for proposers (#110)

--- script/l3/start-proposer.sh
+++ script/l3/start-proposer.sh
@@ -4,9 +4,9 @@ set -eou pipefail
 
 ARGS="--l1.ws ${L2_ENDPOINT_WS}
     --l2.http http://l3_execution_engine:8545
-    --taikoL1 ${TAIKO_L1_ADDRESS} 
-    --taikoL2 ${TAIKO_L2_ADDRESS} 
-    --l1.proposerPrivKey ${L2_PROPOSER_PRIVATE_KEY} 
+    --taikoL1 ${TAIKO_L1_ADDRESS}
+    --taikoL2 ${TAIKO_L2_ADDRESS}
+    --l1.proposerPrivKey ${L2_PROPOSER_PRIVATE_KEY}
     --l2.suggestedFeeRecipient ${L3_SUGGESTED_FEE_RECIPIENT}
     --minimalBlockGasLimit 5000000"
 
@@ -21,6 +21,10 @@ fi
 
 if [[ ! -z "$WAIT_RECEIPT_TIMEOUT" ]]; then
     ARGS="${ARGS} --rpc.waitReceiptTimeout ${WAIT_RECEIPT_TIMEOUT}"
+fi
+
+if [[ ! -z "$PROPOSE_BLOCK_TX_GAS_TIP_CAP" ]]; then
+    ARGS="${ARGS} --proposeBlockTxGasTipCap ${PROPOSE_BLOCK_TX_GAS_TIP_CAP}"
 fi
 
 if [ "$ENABLE_PROPOSER" == "true" ]; then
