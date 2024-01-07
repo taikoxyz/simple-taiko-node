8fe6b77e1121e7cef64dce568e2a6cda1be1894e
David <david@taiko.xyz>
2024-01-07 23:44:00 +0800

    feat: update guardian prover configs

--- script/start-driver.sh
+++ script/start-driver.sh
@@ -2,22 +2,12 @@
 
 set -eou pipefail
 
-if [ "$DISABLE_P2P_SYNC" == "false" ]; then
-    taiko-client driver \
-        --l1.ws ${L1_ENDPOINT_WS} \
-        --l2.ws ws://l2_execution_engine:8546 \
-        --l2.auth http://l2_execution_engine:8551 \
-        --taikoL1 ${TAIKO_L1_ADDRESS} \
-        --taikoL2 ${TAIKO_L2_ADDRESS} \
-        --jwtSecret /data/taiko-geth/geth/jwtsecret \
-        --p2p.syncVerifiedBlocks \
-        --p2p.checkPointSyncUrl https://rpc.katla.taiko.xyz
-else
-    taiko-client driver \
-        --l1.ws ${L1_ENDPOINT_WS} \
-        --l2.ws ws://l2_execution_engine:8546 \
-        --l2.auth http://l2_execution_engine:8551 \
-        --taikoL1 ${TAIKO_L1_ADDRESS} \
-        --taikoL2 ${TAIKO_L2_ADDRESS} \
-        --jwtSecret /data/taiko-geth/geth/jwtsecret
-fi
+taiko-client driver \
+    --l1.ws ${L1_ENDPOINT_WS} \
+    --l2.ws ws://l2_execution_engine:8546 \
+    --l2.auth http://l2_execution_engine:8551 \
+    --taikoL1 ${TAIKO_L1_ADDRESS} \
+    --taikoL2 ${TAIKO_L2_ADDRESS} \
+    --jwtSecret /data/taiko-geth/geth/jwtsecret \
+    --p2p.syncVerifiedBlocks \
+    --p2p.checkPointSyncUrl https://rpc.katla.taiko.xyz
--- script/start-zkevm-chain-rpcd.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -eu pipefail
-
-wget https://storage.googleapis.com/zkevm-circuits-keys/kzg_bn254_22.srs -P /data
-
-/prover_rpcd \
-    --bind 0.0.0.0:9000 \
-    --lookup zkevm_chain_prover_rpcd:9000
