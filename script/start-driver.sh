1ce22213347250e3eb2548841a0cdd1369a2dbef
nakochi <nakochi@kudasaijp.com>
2024-05-03 04:09:37 +0900

    feat: handle unix signals

--- script/start-driver.sh
+++ script/start-driver.sh
@@ -3,7 +3,7 @@
 set -eou pipefail
 
 if [ "$DISABLE_P2P_SYNC" = "false" ]; then
-    taiko-client driver \
+    exec taiko-client driver \
         --l1.ws "${L1_ENDPOINT_WS}" \
         --l2.ws ws://l2_execution_engine:8546 \
         --l1.beacon "${L1_BEACON_HTTP}" \
@@ -14,7 +14,7 @@ if [ "$DISABLE_P2P_SYNC" = "false" ]; then
         --p2p.sync \
         --p2p.checkPointSyncUrl https://rpc.hekla.taiko.xyz
 else
-    taiko-client driver \
+    exec taiko-client driver \
         --l1.ws "${L1_ENDPOINT_WS}" \
         --l2.ws ws://l2_execution_engine:8546 \
         --l1.beacon "${L1_BEACON_HTTP}" \
