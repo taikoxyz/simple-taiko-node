b8d8f80d50fa81e31390246a29650e142e8e4387
Jorge Mederos <jmederosalvarado@gmail.com>
2024-07-02 12:51:50 +0200

    fixes

--- script/start-driver.sh
+++ script/start-driver.sh
@@ -5,12 +5,12 @@ set -eou pipefail
 if [ "$DISABLE_P2P_SYNC" = "false" ]; then
     exec taiko-client driver \
         --l1.ws "${L1_ENDPOINT_WS}" \
-        --l2.ws ws://l2_execution_engine:8546 \
+        --l2.ws ws://l2_execution_engine:8545 \
         --l1.beacon "${L1_BEACON_HTTP}" \
         --l2.auth http://l2_execution_engine:8551 \
         --taikoL1 "${TAIKO_L1_ADDRESS}" \
         --taikoL2 "${TAIKO_L2_ADDRESS}" \
-        --jwtSecret /data/taiko-geth/geth/jwtsecret \
+        --jwtSecret /data/jwtsecret \
         --p2p.sync \
         --p2p.checkPointSyncUrl "${P2P_SYNC_URL}"
 else
@@ -21,5 +21,5 @@ else
         --l2.auth http://l2_execution_engine:8551 \
         --taikoL1 "${TAIKO_L1_ADDRESS}" \
         --taikoL2 "${TAIKO_L2_ADDRESS}" \
-        --jwtSecret /data/taiko-geth/geth/jwtsecret
+        --jwtSecret /data/jwtsecret
 fi
