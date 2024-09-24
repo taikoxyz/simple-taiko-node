8279b8b595adff380eccb15fd10ced35e07a7e3f
gavin <gavin@taiko.xyz>
2024-09-24 10:55:38 +0800

    feat(prover,proposer): bump  client version

--- script/start-prover-relayer.sh
+++ script/start-prover-relayer.sh
@@ -5,7 +5,6 @@ set -eou pipefail
 if [ "$ENABLE_PROVER" = "true" ]; then
     ARGS="--l1.ws ${L1_ENDPOINT_WS}
         --l2.ws ws://l2_execution_engine:8546
-        --l1.http ${L1_ENDPOINT_HTTP}
         --l2.http http://l2_execution_engine:8545
         --taikoL1 ${TAIKO_L1_ADDRESS}
         --taikoL2 ${TAIKO_L2_ADDRESS}
