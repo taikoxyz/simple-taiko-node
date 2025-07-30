4ecf454d844e03740035c88253127b7d42f6b36b
Roger <50648015+RogerLamTd@users.noreply.github.com>
2025-07-29 17:08:06 -0700

    chore(driver): update hekla p2p bootnodes (#401)

--- script/start-driver-hekla.sh
+++ script/start-driver-hekla.sh
@@ -2,7 +2,7 @@
 
 set -eou pipefail
 
-P2P_BOOTNODES="enr:-J64QDvU6s4LuxAegET9xidBag2zHNzBuU6cZ1IPStGx_U2nCU8vR8EZxjHmQYCfH08PJd_9BQErXfStUpiofXIRVquGAZcyLG_hgmlkgnY0gmlwhCIuHGOHb3BzdGFja4ThmAoAiXNlY3AyNTZrMaEC6luKeXmF9QCvo3ugPOR7ADl5KpQvCsm-6foZp6VBAnODdGNwgg-hg3VkcIK5ZA,enr:-J64QO9o3nZi9CPGlriurNJGJ7AttHdT6CuyHDM5QTVgHgm_LMy3ngOk47ENaRFfpiWWEUFDyEKYj9V60Ckhl84hvk-GAZcyLHhxgmlkgnY0gmlwhCIej3yHb3BzdGFja4ThmAoAiXNlY3AyNTZrMaEDQE6UkwZhB0Mb3z9Hu7Ngo1MkSmBpkDx2ko_KMGdXXyqDdGNwgg-hg3VkcIKx3A,enr:-J64QMhRPUng6b6X-1cDLc4QKiZMmBQJWqZtPY77LyFHp4hJfr9a7nO_-VjJy4uyHGiFb76Ke3IZIqcvSFRjhxbLDXWGAZcyKpHDgmlkgnY0gmlwhCI4NgiHb3BzdGFja4ThmAoAiXNlY3AyNTZrMaEDstCUreHOaJkIeLGXv4GL1B1bMuQBnULcY9kdcCD1MVODdGNwgg-hg3VkcIKGpA"
+P2P_BOOTNODES="enode://ea5b8a797985f500afa37ba03ce47b0039792a942f0ac9bee9fa19a7a5410273fe43b4e8a9a28fa42cdec1b6435deb809fcb79479c55cc2ddbaf02de7a83f456@34.46.28.99:4001?discport=30304,enode://404e9493066107431bdf3f47bbb360a353244a6069903c76928fca3067575f2adf62ef396b0a8a74696669ef53cab8373e38ddd9b3d3d2d76f356d2cd2708951@34.30.143.124:4001?discport=30304,enode://b2d094ade1ce68990878b197bf818bd41d5b32e4019d42dc63d91d7020f531532f063833e44e84f78792969f69fd5553370216703c40fcc0cb835f17177fbe07@34.56.54.8:4001?discport=30304"
 
 ARGS="--l1.ws ${L1_ENDPOINT_WS} \
     --l2.ws ws://l2_execution_engine:8546 \
@@ -10,13 +10,13 @@ ARGS="--l1.ws ${L1_ENDPOINT_WS} \
     --l2.auth http://l2_execution_engine:8551 \
     --taikoInbox ${TAIKO_INBOX_ADDRESS} \
     --taikoAnchor ${TAIKO_ANCHOR_ADDRESS} \
+    --verbosity ${VERBOSITY} \
     --preconfirmation.whitelist ${PRECONFIRMATION_WHITELIST} \
     --preconfirmation.serverPort 9871 \
     --jwtSecret /data/taiko-geth/geth/jwtsecret \
     --p2p.bootnodes ${P2P_BOOTNODES} \
     --p2p.listen.ip 0.0.0.0 \
-    --p2p.useragent taiko \
-    --p2p.listen.tcp ${P2P_LISTEN_TCP_PORT}"
+    --p2p.useragent taiko"
 
 if [ "$DISABLE_P2P_SYNC" = "false" ]; then
     ARGS="${ARGS} --p2p.sync \
@@ -24,7 +24,11 @@ if [ "$DISABLE_P2P_SYNC" = "false" ]; then
 fi
 
 if [ -n "$PUBLIC_IP" ]; then
-    ARGS="${ARGS} --p2p.advertise.ip ${PUBLIC_IP}"
+    ARGS="${ARGS} --p2p.advertise.ip ${PUBLIC_IP} \
+    --p2p.advertise.udp ${P2P_UDP_PORT} \
+    --p2p.listen.udp ${P2P_UDP_PORT} \
+    --p2p.advertise.tcp ${P2P_TCP_PORT} \
+    --p2p.listen.tcp ${P2P_TCP_PORT}"
 else
     ARGS="${ARGS} --p2p.nat"
 fi
