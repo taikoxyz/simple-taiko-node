b978691de7a0d9579dea18fe33f528efc334ae5d
Gavin Yu <gavin@taiko.xyz>
2026-06-12 11:55:20 +0800

    feat(driver): upgrade Hoodi node to alethia-reth with Unzen fork (#449)
    * chore: upgrade hoodi to alethia reth
    
    * chore: restore hoodi p2p bootnodes
    
    * chore: restore hoodi env-backed driver options
    
    * chore: remove hoodi preconfirmation whitelist
    
    * chore: restore hoodi p2p preconf options
    
    * Apply suggestion from @YoGhurt111
    
    * chore: remove hoodi execution p2p port config
    
    * fix
    
    * chore: allow hoodi reth snapshot data dir
    
    * fix comments
    
    * fix: share hoodi reth data dir with driver
    
    * update hoodi bootnodes
    
    * add geth_execution_engine back
    
    * rename
    
    * Update docker-compose-hoodi.yml
    
    * Update docker-compose-hoodi.yml
    
    * Update docker-compose-hoodi.yml
    
    * docs: clarify hoodi reth snapshot data dir
    
    ---------
    
    Co-authored-by: RogerLamTd <bob.rogerbob1@gmail.com>

--- script/start-driver-hoodi.sh
+++ script/start-driver-hoodi.sh
@@ -4,17 +4,20 @@ set -eou pipefail
 
 P2P_BOOTNODES="enode://ea5b8a797985f500afa37ba03ce47b0039792a942f0ac9bee9fa19a7a5410273fe43b4e8a9a28fa42cdec1b6435deb809fcb79479c55cc2ddbaf02de7a83f456@35.239.142.239:4001?discport=30304,enode://404e9493066107431bdf3f47bbb360a353244a6069903c76928fca3067575f2adf62ef396b0a8a74696669ef53cab8373e38ddd9b3d3d2d76f356d2cd2708951@34.63.118.244:4001?discport=30304,enode://b2d094ade1ce68990878b197bf818bd41d5b32e4019d42dc63d91d7020f531532f063833e44e84f78792969f69fd5553370216703c40fcc0cb835f17177fbe07@34.63.52.157:4001?discport=30304"
 
+JWT_SECRET="/data/alethia-reth/jwtsecret"
+
+if [ "${COMPOSE_PROFILES:-}" = "geth_execution_engine" ]; then
+  JWT_SECRET="/data/taiko-geth/geth/jwtsecret"
+fi
+
 ARGS="--l1.ws ${L1_ENDPOINT_WS} \
     --l2.ws ws://l2_execution_engine:8546 \
     --l1.beacon ${L1_BEACON_HTTP} \
     --l2.auth http://l2_execution_engine:8551 \
-    --pacayaInbox ${TAIKO_INBOX_ADDRESS} \
-    --shastaInbox ${SHASTA_INBOX_ADDRESS} \
-    --shasta.time ${TAIKO_INTERNAL_SHASTA_TIME} \
+    --inbox ${TAIKO_INBOX_ADDRESS} \
     --taikoAnchor ${TAIKO_ANCHOR_ADDRESS} \
     --verbosity ${VERBOSITY} \
-    --preconfirmation.whitelist ${PRECONFIRMATION_WHITELIST} \
-    --jwtSecret /data/taiko-geth/geth/jwtsecret"
+    --jwtSecret ${JWT_SECRET}"
 
 if [ -n "$BLOB_SERVER_URL" ]; then
     ARGS="${ARGS} --blob.server ${BLOB_SERVER_URL}"
@@ -29,6 +32,7 @@ if [ "$ENABLE_PRECONFS_P2P" = "true" ]; then
   ARGS="${ARGS} --p2p.peerstore.path /node-keys/peerstore \
       --p2p.discovery.path /node-keys/discv5 \
       --preconfirmation.serverPort 9871 \
+      --preconfirmation.jwtSecret ${JWT_SECRET} \
       --p2p.listen.ip 0.0.0.0 \
       --p2p.useragent taiko \
       --p2p.bootnodes ${P2P_BOOTNODES}"
